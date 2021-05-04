403b7adf3ab199e99e3e1aaf41c751092f05b6f1
lentinip <lentinip@users.noreply.github.com>
2021-05-04 17:05:43 +0200

    First try
    Added style and menu bar

---
+++ hypermood/components/MenuBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nav class="navbar navbar-light bg-light static-top">
+      <div class="container">
+        <a class="navbar-brand" href="#!">Start Bootstrap</a>
+        <a class="btn btn-primary" href="#!">Sign In</a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- hypermood/nuxt.config.js
+++ hypermood/nuxt.config.js
@@ -14,7 +14,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    // CSS file in the project
+    '@/css/style.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
