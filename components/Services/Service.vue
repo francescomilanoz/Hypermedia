86547f6b696a096d0ab1a73ecbd7cced8a9a354a
lentinip <lentinip@users.noreply.github.com>
2021-05-20 00:35:05 +0200

    Implemented Services page with example data

---
+++ components/Services/Service.vue
@@ -0,0 +1,57 @@
+<template>
+  <nuxt-link :to="'/services/' + id">
+    <div class="container">
+      <h1>{{ name }}</h1>
+      <p>{{ previewText }}</p>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'Service',
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    previewText: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+h1 {
+  font-weight: bold;
+  font-size: 22px;
+  line-height: 24px;
+  text-align: center;
+}
+p {
+  font-weight: normal;
+  font-size: 18px;
+  /* line-height: 24px; */
+  text-align: center;
+}
+
+a {
+  text-decoration: none;
+}
+
+.container {
+  color: var(--blue);
+  /* margin: auto; */
+  width: 100%;
+  height: auto;
+  /* Uncomment this below to help you visualize the element */
+  /* border-width: 1px;
+  border-style: solid; */
+}
+</style>
