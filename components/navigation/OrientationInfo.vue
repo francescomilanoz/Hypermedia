e965791e6c6fe76d1542e033751688cafb8ff955
francescomilanoz <francescomilanozzz@gmail.com>
2021-05-24 16:30:10 +0200

    minor changes

--- components/navigation/OrientationInfo.vue
+++ components/navigation/OrientationInfo.vue
@@ -1,169 +1,51 @@
 <template>
-		
-		
-			 
-			<div>
-		
-		
-			 
-			<nuxt-link :to="'/' + sectionName">
-		
-		
-			 
-			<div>
-		
-		
-			 
-			<h1 class="back-arrow">&#8249;</h1>
-		
-		
-			 
-			<h1>{{ text }}</h1>
-		
-		
-			 
-			</div>
-		
-		
-			 
-			</nuxt-link>
-		
-		
-			 
-			</div>
-		
-		
-			 
-			</template>
-		
-		
-			 
-			 
-		
-		
-			 
-			<script>
-		
-		
-			 
-			export default {
-		
-		
-			 
-			name: 'OrientationInfo',
-		
-		
-			 
-			props: {
-		
-		
-			 
-			text: {
-		
-		
-			 
-			type: String,
-		
-		
-			 
-			required: true,
-		
-		
-			 
-			},
-		
-		
-			 
-			sectionName: {
-		
-		
-			 
-			type: String,
-		
-		
-			 
-			required: true,
-		
-		
-			 
-			},
-		
-		
-			 
-			},
-		
-		
-			 
-			}
-		
-		
-			 
-			</script>
-		
-		
-			 
-			 
-		
-		
-			 
-			<style scoped>
-		
-		
-			 
-			a {
-		
-		
-			 
-			text-decoration: none;
-		
-		
-			 
-			color: var(--blue);
-		
-		
-			 
-			}
-		
-		
-			 
-			 
-		
-		
-			 
-			h1 {
-		
-		
-			 
-			display: inline;
-		
-		
-			 
-			font-weight: normal;
-		
-		
-			 
-			}
-		
-		
-			 
-			 
-		
-		
-			 
-			.back-arrow {
-		
-		
-			 
-			font-size: 30px;
-		
-		
-			 
-			vertical-align: top;
-		
-		
-			 
-			}
-		
-		
-			 
-			</style>
+  <div>
+    <nuxt-link :to="'/' + sectionName">
+      <div>
+        <h1 class="back-arrow">&#8249;</h1>
+
+        <h1>{{ text }}</h1>
+      </div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OrientationInfo',
+
+  props: {
+    text: {
+      type: String,
+
+      required: true,
+    },
+
+    sectionName: {
+      type: String,
+
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+
+  color: var(--blue);
+}
+
+h1 {
+  display: inline;
+
+  font-weight: normal;
+}
+
+.back-arrow {
+  font-size: 30px;
+
+  vertical-align: top;
+}
+</style>
