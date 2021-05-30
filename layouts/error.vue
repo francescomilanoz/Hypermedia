61257b8fc4134fbabcd5b60bd29c7bbad4df6b59
francescomilanoz <francescomilanozzz@gmail.com>
2021-05-30 16:26:09 +0200

    Added 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Cover
+      title="PAGE NOT FOUND!"
+      preview-text="Sorry, we couldn't find the page you requested. You can click on one of the links above to navigate to a correct page."
+      thumbnail="https://i.pinimg.com/736x/a2/25/30/a225306eaa8e89808ab4557596f3aa25.jpg"
+    />
+  </div>
+</template>
